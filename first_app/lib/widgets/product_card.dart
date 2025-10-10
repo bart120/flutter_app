6b8a7c6c4d6408f0097713e43f5a10e4ea837e1e
Vincent Leclerc <vleclerc@inow.fr>
2025-10-10 12:19:35 +0200

    product list (3)

--- first_app/lib/widgets/product_card.dart
+++ first_app/lib/widgets/product_card.dart
@@ -21,7 +21,47 @@ class ProductCard extends StatelessWidget {
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [],
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadiusGeometry.vertical(
+                top: Radius.circular(16),
+              ),
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) =>
+                    const Center(child: Icon(Icons.image_not_supported)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.name,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  '${product.price} €',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: colorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
